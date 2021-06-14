88228844085770be2c5f0cb76d723a942802af27
Mustafa Kahraman <80291704+kahramanX@users.noreply.github.com>
2021-06-14 01:17:32 +0000

    average of 3 numbers :p

---
+++ average-of-3-numbers.js
@@ -0,0 +1,11 @@
+//average of 3 numbers
+let num1 = parseInt(prompt("1. sayıyı giriniz..."));
+let num2 = parseInt(prompt("2. sayıyı giriniz..."));
+let num3 = parseInt(prompt("3. sayıyı giriniz..."));
+
+let average = num1+num2+num3;
+
+let result = average/3;
+
+
+console.log("3 sayının ortalaması: "+result);
